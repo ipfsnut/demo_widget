762a4e3e347851ca8ae9efdb7aaec0e0c628a133
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2024-07-15 12:06:57 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import DAOWidget from './DAOWidget';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DAO Widget Demo</h1>
+      </header>
+      <main>
+        <DAOWidget />
+      </main>
+    </div>
+  );
+}
+
+export default App;
